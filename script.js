77d94d821a8f0caeaa1f0b90e64d0c20f5fb789e
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-04-24 11:22:00 -0300

    add - adicionei a primeira parte da validacao no javascript.

---
+++ script.js
@@ -0,0 +1,26 @@
+let form = document.getElementById('formulario');
+
+let dados = [];
+
+form.addEventListener('submit', function(e){
+
+    e.preventDefault();
+
+    let valido = true;
+
+    let nome = document.getElementById('nome').value;
+    let cpf = document.getElementById('cpf').value;
+    let dataNascimento = document.getElementById('dataNascimento').value;
+    let telefone = document.getElementById('telefone').value;
+    let email = document.getElementById('email').value;
+    let endereco = document.getElementById('endereco').value;
+    let cidade = document.getElementById('cidade').value;
+    let estado = document.getElementById('estado').value;
+    let cep = document.getElementById('cep').value;
+    let curso = document.getElementById('curso').value;
+    let matricula = document.getElementById('matricula').value;
+    let senha = document.getElementById('senha').value;
+    let confirmar = document.getElementById('confirmar').value;
+
+
+})
